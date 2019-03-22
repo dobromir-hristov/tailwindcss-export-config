349d4f260991d4f3be91dc654d1215ee46994478
Jordy Pereira <jordy.pereira@prorenata.se>
2019-03-22 17:28:05 +0100

    :white_check_mark: Added Less test if array converts to comma seperated string
    Take the first array key in the testobject.
    Converts that to a list and checks if it's in the converted testObject.

--- tests/specs/converters/Less.spec.js
+++ tests/specs/converters/Less.spec.js
@@ -1,5 +1,7 @@
 const LessConverter = require('../../../src/converters/Less')
 const testConfig = require('../../tailwind.config')
+const variablePrefix = '@'
+const variableSuffix = ';'
 
 describe('Less converter', () => {
   it('Converts to nested map', () => {
@@ -32,5 +34,17 @@ describe('Less converter', () => {
       prefix: 'tw'
     })
     expect(converter.convert()).toMatchSnapshot()
+  })
+
+  it('Converts an array to a comma separated string', () => {
+    const testObject = 'fonts'
+    const keyName = Object.keys(testConfig[testObject])[0];
+    const values = testConfig[testObject][keyName].join(', ')
+    const expectedResult = `${variablePrefix}${testObject}-${keyName}: ${values}${variableSuffix}`
+
+    const converter = new LessConverter({
+      config: testConfig,
+    })
+    expect(converter.convert()).toContain(expectedResult)
   })
 })
